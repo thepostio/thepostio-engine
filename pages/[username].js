d82db73186fd1bcb7c33f9cf94a1ca36ef672568
jonathanlurie <lurie.jo@gmail.com>
2020-09-27 17:17:46 +0200

    ADD load article cards clienbt side

--- pages/[username].js
+++ pages/[username].js
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router'
+import { withRouter } from 'next/router'
 import { Breadcrumb, Tooltip, Button, Divider, Col, Row, Space } from 'antd'
 import {
   TwitterCircleFilled,
@@ -15,159 +15,220 @@ import { getAuthorData, getPostMetadata } from '../server/data'
 import PostCard from '../components/PostCard'
 import styles from './username.module.css'
 
+// how many articles on each load
+const ARTICLE_PAGE_SIZE = 1
 
-const User = ({userData, articleMetas}) => {
-  const router = useRouter()
-  const { username } = router.query
+class User extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      articleMetas: []
+    }
+  }
+
+
+  componentDidMount() {
+    this.loadNextArticles()
+  }
+
+  // load the next batch of articles
+  loadNextArticles = async () => {
+    const articleMetas = this.state.articleMetas
+    const postIds = this.props.userData.data.articles
+    const provider = this.props.provider
+    const router = this.props.router
+    const { username } = router.query
+
+    const postIdsToLoad = postIds.slice(articleMetas.length, articleMetas.length + ARTICLE_PAGE_SIZE)
+
+    const promises = postIdsToLoad.map((postId) => fetch(`/api/articlemeta?username=${username}&postid=${postId}&provider=${provider}`))
+
+    const res = await Promise.all(promises)
+    const newArticleMetasCandidates = await Promise.all(res.map((r) => r.json()))
+    const newArticleMetas = newArticleMetasCandidates.filter((meta) => !meta.error)
+
+    const erroredArticles = newArticleMetasCandidates.filter((meta) => meta.error)
+
+    // TODO: notification for the missing articles
+    // TODO: in case of error, make sure we dont display doublons
+
+    this.setState({
+      articleMetas: articleMetas.concat(newArticleMetas)
+    })
+  }
+
+
+  render() {
+    const router = this.props.router
+    const { username } = router.query
+    const userData = this.props.userData
+    const articleMetas = this.state.articleMetas //this.props.articleMetas
+    const provider = this.props.provider
+    const postIds = this.props.userData.data.articles
   
+    console.log('userData', userData)
 
-  if (userData.error) {
-    return (
-      <MainLayout>
-        <div
-          style={{
-            textAlign: 'center',
-            color: '#ffbb74',
-            marginTop: '3em',
-          }}
-        >
-          <FrownOutlined style={{fontSize: '7em'}}/>
-          <div style={{
-              fontSize: '1em',
-              marginTop: '2em',
+
+    if (userData.error) {
+      return (
+        <MainLayout>
+          <div
+            style={{
+              textAlign: 'center',
+              color: '#ffbb74',
+              marginTop: '3em',
             }}
           >
-            {userData.error}
+            <FrownOutlined style={{fontSize: '7em'}}/>
+            <div style={{
+                fontSize: '1em',
+                marginTop: '2em',
+              }}
+            >
+              {userData.error}
+            </div>
           </div>
+        </MainLayout>
+      )
+    }
+  
+    const userLinks = []
+  
+    if (userData.data.author.website) {
+      userLinks.push(
+        <Tooltip title='Visit website' color='blue' key='website'>
+          <a style={{fontSize: '2em'}} href={userData.data.author.website}><GlobalOutlined /></a>
+        </Tooltip>
+      )
+    }
+  
+    if (userData.data.author.twitter) {
+      userLinks.push(
+        <Tooltip title='See Twitter profile' color='blue' key='twitter'>
+          <a style={{fontSize: '2em'}} href={`https://twitter.com/${userData.data.author.twitter}`}><TwitterCircleFilled /></a>
+        </Tooltip>
+      )
+    }
+  
+    if (userData.data.author.github) {
+      userLinks.push(
+        <Tooltip title='See GitHub profile' color='blue' key='github'>
+          <a style={{fontSize: '2em'}} href={`https://github.com/${userData.data.author.github}`}><GithubFilled /></a>
+        </Tooltip>
+      )
+    }
+  
+    const headMeta = {
+      title: `The Post — ${userData.data.author.displayName}`,
+      author: userData.data.author.displayName,
+      description: `${articleMetas.length} article${articleMetas.length > 1 ? 's' : ''} by ${userData.data.author.displayName} on The Post`,
+      cover: userData.data.author.picture,
+      url: `https://thepost.io/${username}`,
+    }
+  
+    return (
+      <MainLayout headMeta={headMeta}>
+        <div>
+        <Divider>
+  
+          <Breadcrumb
+            className={styles.breadcrumbs}
+          >
+            <Breadcrumb.Item>
+              <Link href='/'><a>The Post</a></Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+            {userData.data.author.displayName}
+            </Breadcrumb.Item>
+          </Breadcrumb>
+  
+          </Divider>
+  
+          <Row
+            style={{
+              marginTop: '1em',
+            }}
+          >
+            <Col xs={5}  sm={7}  md={7}  lg={8} xl={9}>
+            </Col>
+  
+            <Col xs={14}  sm={10}  md={10}  lg={8} xl={6}>
+              <img
+                className={styles.profilepicture}
+                src={userData.data.author.picture}
+              />
+            </Col>
+  
+            <Col xs={5}  sm={7}  md={7}  lg={8} xl={9}>
+            </Col>
+          </Row>
+  
+  
+          <Row
+            style={{
+              textAlign: 'center',
+              marginTop: '1em'
+            }}
+          >
+            <Col xs={2}  sm={2}  md={5}  lg={6} xl={6}>
+            </Col>
+  
+            <Col xs={20}  sm={20}  md={14}  lg={12} xl={12}>
+              <h1
+                className={styles.username}
+              >
+                {userData.data.author.displayName}
+              </h1>
+  
+              {
+                userLinks.length 
+                ? 
+                (
+                  <Space
+                    className={styles.sociallinks}
+                  >
+                    {userLinks}
+                  </Space>
+                )
+                : null
+              }
+  
+              <p
+                className={styles.biography}
+              >
+              {userData.data.author.biography}
+              </p>
+            </Col>
+  
+            <Col xs={2}  sm={2}  md={5}  lg={6} xl={6}>
+            </Col>
+          </Row>
+  
+          <div>
+            <Space direction="vertical" size={75} style={{width: '100%', marginTop: 50}}>
+              {articleMetas.map((meta, i) => <PostCard key={i} postMetadata={meta}/>) }
+            </Space>
+          </div>
+
+          {
+            articleMetas.length < postIds.length
+            ? 
+            <div className={styles.loadmorecontainer}>
+              <Button type="primary" onClick={this.loadNextArticles}>Load more</Button>
+            </div>
+            : null
+          }
+  
         </div>
       </MainLayout>
     )
   }
 
-  const userLinks = []
 
-  if (userData.data.author.website) {
-    userLinks.push(
-      <Tooltip title='Visit website' color='blue' key='website'>
-        <a style={{fontSize: '2em'}} href={userData.data.author.website}><GlobalOutlined /></a>
-      </Tooltip>
-    )
-  }
-
-  if (userData.data.author.twitter) {
-    userLinks.push(
-      <Tooltip title='See Twitter profile' color='blue' key='twitter'>
-        <a style={{fontSize: '2em'}} href={`https://twitter.com/${userData.data.author.twitter}`}><TwitterCircleFilled /></a>
-      </Tooltip>
-    )
-  }
-
-  if (userData.data.author.github) {
-    userLinks.push(
-      <Tooltip title='See GitHub profile' color='blue' key='github'>
-        <a style={{fontSize: '2em'}} href={`https://github.com/${userData.data.author.github}`}><GithubFilled /></a>
-      </Tooltip>
-    )
-  }
-
-  const headMeta = {
-    title: `The Post — ${userData.data.author.displayName}`,
-    author: userData.data.author.displayName,
-    description: `${articleMetas.length} article${articleMetas.length > 1 ? 's' : ''} by ${userData.data.author.displayName} on The Post`,
-    cover: userData.data.author.picture,
-    url: `https://thepost.io/${username}`,
-  }
-
-  return (
-    <MainLayout headMeta={headMeta}>
-      <div>
-      <Divider>
-
-        <Breadcrumb
-          className={styles.breadcrumbs}
-        >
-          <Breadcrumb.Item>
-            <Link href='/'><a>The Post</a></Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-          {userData.data.author.displayName}
-          </Breadcrumb.Item>
-        </Breadcrumb>
-
-        </Divider>
-
-        <Row
-          style={{
-            marginTop: '1em',
-          }}
-        >
-          <Col xs={5}  sm={7}  md={7}  lg={8} xl={9}>
-          </Col>
-
-          <Col xs={14}  sm={10}  md={10}  lg={8} xl={6}>
-            <img
-              className={styles.profilepicture}
-              src={userData.data.author.picture}
-            />
-          </Col>
-
-          <Col xs={5}  sm={7}  md={7}  lg={8} xl={9}>
-          </Col>
-        </Row>
-
-
-        <Row
-          style={{
-            textAlign: 'center',
-            marginTop: '1em'
-          }}
-        >
-          <Col xs={2}  sm={2}  md={5}  lg={6} xl={6}>
-          </Col>
-
-          <Col xs={20}  sm={20}  md={14}  lg={12} xl={12}>
-            <h1
-              className={styles.username}
-            >
-              {userData.data.author.displayName}
-            </h1>
-
-            {
-              userLinks.length 
-              ? 
-              (
-                <Space
-                  className={styles.sociallinks}
-                >
-                  {userLinks}
-                </Space>
-              )
-              : null
-            }
-
-            <p
-              className={styles.biography}
-            >
-            {userData.data.author.biography}
-            </p>
-          </Col>
-
-          <Col xs={2}  sm={2}  md={5}  lg={6} xl={6}>
-          </Col>
-        </Row>
-
-        <div>
-          <Space direction="vertical" size={75} style={{width: '100%', marginTop: 50}}>
-            {articleMetas.map((meta, i) => <PostCard key={i} postMetadata={meta}/>) }
-          </Space>
-        </div>
-
-      </div>
-    </MainLayout>
-  )
 }
 
-export default User
+export default withRouter(User)
 
 
 export async function getServerSideProps(context) {
@@ -188,6 +249,7 @@ export async function getServerSideProps(context) {
     props: {
       userData,
       articleMetas,
+      provider,
     },
   }
 }
---
+++ pages/api/articlemeta.js
@@ -0,0 +1,52 @@
+import { getPostMetadata } from '../../server/data'
+/**
+ * Here we are counting the visit to a specific post.
+ * The provider + author username + postId are included in the token (JWT)
+ * so the request has basically no (need for a) body
+ */
+export default async (req, res) => {
+  let visitToken = null
+  const query = req.query
+
+  let retValue = {
+    data: null,
+    error: null
+  }
+
+  try {
+    let username = null
+    let postId = null
+    let provider = 'github'
+
+    if ('username' in query) {
+      username = query.username
+    } else {
+      throw new Error('The username must be provided')
+    }
+
+    if ('postid' in query) {
+      postId = query.postid
+    } else {
+      throw new Error('The postid must be provided')
+    }
+
+    if ('provider' in query) {
+      provider = query.provider
+    }
+
+    let metadata = await getPostMetadata(username, postId, provider)
+
+    if (metadata.error) {
+      throw new Error(metadata.error)
+    } else {
+      retValue.data = metadata.data
+    }
+
+  } catch (err) {
+    retValue.error = err.message
+  }
+  
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(retValue))
+}
